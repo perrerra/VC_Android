6bedb8047eafebbe97f866a09c16c2937a6107d9
Guillaume Tostain <guillaumetostain@gtostain.local>
2017-05-12 10:22:15 +0200

    Initial commit

---
+++ app/src/main/java/com/mobile/veloconnecte/vcandroid/activities/BikeListActivity.java
@@ -0,0 +1,15 @@
+package com.mobile.veloconnecte.vcandroid.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mobile.veloconnecte.vcandroid.R;
+
+public class BikeListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bike_list);
+    }
+}
